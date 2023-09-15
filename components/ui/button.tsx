ef6cb8bed8e8e1700c6f59b5a04fe22e86b54e82
Ayubu Kayange <kayangejr3@gmail.com>
2023-09-15 18:18:49 +0300

    minor changes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,6 +14,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           "w-auto rounded-xl flex bg-black border-transparent p-2.5 disabled:opacity-50 disabled:cursor-not-allowed text-white font-semibold transition hover:opacity-75",
           className
         )}
+        disabled={disabled}
         {...props}
       >
         {children}
