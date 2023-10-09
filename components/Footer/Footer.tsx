deba5a1229fff7da3ac0e0390ebf5211d303d122
Ayubu Kayange <kayangejr3@gmail.com>
2023-10-09 13:21:48 +0300

    updated footer component

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,20 +1,23 @@
+import { Globe } from "lucide-react";
 import Link from "next/link";
 
 const Footer = () => {
-    const year = new Date().getFullYear();
-    return (
-        <footer className="bg-white border-t w-full">
-            <div className="py-10 flex w-full items-center justify-evenly">
-                <p className="text-sm text-black">
-                    &copy; {year} Baimi-ecommerce store. All rights reserved.
-                </p> 
-                <p>
-                    <span>powered by </span>
-                    <Link href="https:bit.ly/kayange" target="_blank" className="underline text-lg text-blue-900">kayangeInc</Link>
-                </p>
-            </div>
-        </footer>
-     );
-}
- 
+  const year = new Date().getFullYear();
+  return (
+    <footer className="bg-white border-t w-full">
+      <div className="p-10 flex flex-col md:flex-row w-full items-center justify-center space-x-3">
+        <p className="text-xs md:text-sm text-slate-500">
+          &copy; {year} Baimi-ecommerce store. All rights reserved.
+        </p>
+        <p>
+          <Link rel="noopener" href="https://bit.ly/kayange" target="_blank">
+            <span>powered by</span>
+            <Globe className="h-4 w-4" />
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
