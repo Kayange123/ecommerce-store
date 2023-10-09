2746f55424ec8ab836f13253b1b36b0a0d87b53f
Ayubu Kayange <kayangejr3@gmail.com>
2023-10-09 13:49:23 +0300

    minor fixes

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -8,46 +8,44 @@ import NoResults from "@/components/ui/NoResults";
 
 export const revalidate = 0;
 interface CategoryPageProps {
-    params: {
-        categoryId : string;
-    },
-    searchParams: {
-        sizeId: string;
-    }
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    sizeId: string;
+  };
 }
 
-const CategoryPage = async ({params, searchParams}:CategoryPageProps) => {
-    const products = await getProducts({
-        sizeId: searchParams.sizeId,
-        categoryId: params.categoryId
-    });
-    const sizes = await getSizes();
-    const category = await getCategory(params.categoryId);
+const CategoryPage = async ({ params, searchParams }: CategoryPageProps) => {
+  const products = await getProducts({
+    sizeId: searchParams.sizeId,
+    categoryId: params.categoryId,
+  });
+  const sizes = await getSizes();
+  const category = await getCategory(params.categoryId);
   return (
-
     <div className="bg-white">
-        <Container>
-            <Billboard data={category.billboard} />
-            <div className="px-4 sm:px-6 lg:px-8 pb-20">
-                <div className="lg:grid lg:gap-x-8 lg:grid-cols-5">
-                    <MobileFilters sizes={sizes} />
-                    <div className="hidden lg:block">
-                        <Filters valueKey="sizeId" name="sizes" data={sizes} />
-                    </div>
-                    <div className="mt-6 lg:col-span-4 lg:mt-0">
-                        {products.length === 0 && <NoResults />}
-                        <div className="grid grid-cols-3 lg:grid-cols-5 gap-4">
-                            {products.map((product)=>(
-                                <ProductCard product={product} key={product?.id} />
-                            ))}
-                        </div>
-                    </div>
-                </div>
+      <Container>
+        <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-20 p-4">
+          <div className="lg:grid lg:gap-x-8 lg:grid-cols-5">
+            <MobileFilters sizes={sizes} />
+            <div className="hidden lg:block">
+              <Filters valueKey="sizeId" name="sizes" data={sizes} />
             </div>
-        </Container>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-3 lg:grid-cols-5 gap-4">
+                {products.map((product) => (
+                  <ProductCard product={product} key={product?.id} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-
-export default CategoryPage
+export default CategoryPage;
